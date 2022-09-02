a653a78e3074b3671bf58d9d4076db9938168753
sarkymarky <110876020+sarkymarky@users.noreply.github.com>
2022-09-02 01:13:21 -0700

    Delete startGame.js

--- startGame.js
+++
@@ -1,7 +0,0 @@
-function startGame() {
-  startButton.classList.add('hide');
-  shuffledQuestions = questions.sort(() => Math.random() - .5);
-  currentQuestionIndex = 0;
-  questionContainerElement.classList.remove('hide');
-  setNextQuestion();
-}
