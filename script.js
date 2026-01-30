de545b6ca00b3a81ea4df71d2dbff0b5e3188c69
sarkymarky <markjsantuyo@gmail.com>
2026-01-29 22:33:16 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,116 +1,106 @@
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
-
-
-  let shuffledQuestions, currentQuestionIndex
-
-  function startGame() {
-    startButton.classList.add('hide');
-    shuffledQuestions = questions.sort(() => Math.random() - .5);
-    currentQuestionIndex = 0;
-    questionContainerElement.classList.remove('hide');
-    setNextQuestion();
-  }
-  
-
-startButton.addEventListener('click', startGame)
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+let shuffledQuestions, currentQuestionIndex;
+startButton.addEventListener('click', startGame);
 nextButton.addEventListener('click', () => {
-  currentQuestionIndex++
-  setNextQuestion()
-})
-
-document.body.style.backgroundImage="url('https://tenor.com/view/heart-gif-12057137833421676440')"
-
+  currentQuestionIndex++;
+  setNextQuestion();
+});
+document.body.style.backgroundImage = "url('https://tenor.com/view/heart-gif-12057137833421676440')";
+function startGame() {
+  startButton.classList.add('hide');
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove('hide');
+  setNextQuestion();
+}
 function setNextQuestion() {
-  resetState()
-  showQuestion(shuffledQuestions[currentQuestionIndex])
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
-
 function showQuestion(question) {
-  questionElement.innerText = question.question
+  questionElement.innerText = question.question;
   question.answers.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
+    const button = document.createElement('button');
+    button.innerText = answer.text;
+    button.classList.add('btn');
     if (answer.correct) {
-      button.dataset.correct = answer.correct
+      button.dataset.correct = answer.correct;
     }
-    button.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild(button)
-  })
+    button.addEventListener('click', selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
 }
-
 function resetState() {
-  clearStatusClass(document.body)
-  nextButton.classList.add('hide')
+  clearStatusClass(document.body);
+  nextButton.classList.add('hide');
   while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
   }
 }
-
 function selectAnswer(e) {
-  const selectedButton = e.target
-  const correct = selectedButton.dataset.correct
-  setStatusClass(document.body, correct)
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct === "true";
+  setStatusClass(document.body, correct);
   Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
-  })
+    setStatusClass(button, button.dataset.correct === "true");
+  });
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
+    nextButton.classList.remove('hide');
   } else {
-    startButton.innerText = 'Restart'
-    startButton.classList.remove('hide')
+    startButton.innerText = 'Restart';
+    startButton.classList.remove('hide');
   }
 }
-
 function setStatusClass(element, correct) {
-  clearStatusClass(element)
+  clearStatusClass(element);
   if (correct) {
-    element.classList.add('correct')
+    element.classList.add('correct');
   } else {
-    element.classList.add('wrong')
+    element.classList.add('wrong');
   }
 }
-
 function clearStatusClass(element) {
-  element.classList.remove('correct')
-  element.classList.remove('wrong')
+  element.classList.remove('correct');
+  element.classList.remove('wrong');
 }
-
-var count = 15;
-var interval = setInterval(function(){
-  document.getElementById('count').innerHTML=count;
+// 15-second countdown timer
+let count = 15;
+const interval = setInterval(function () {
+  document.getElementById('count').innerHTML = count;
   count--;
-  if (count === 0){
+  if (count === 0) {
     clearInterval(interval);
-    document.getElementById('count').innerHTML='Done';
-    // or...
+    document.getElementById('count').innerHTML = 'Done';
     alert("You're out of time!");
   }
 }, 1000);
-
+// ✅ Fixed questions array
 const questions = [
   {
-    question: "would you be my valentines?",
+    question: "Would you be my Valentine?",
     answers: [
-      { text: "Tony Stark", correct: yes },
-      { text: "Tony Stank", correct: No }
+      { text: "Yes 💕", correct: true },
+      { text: "No 💔", correct: false }
     ]
   },
   {
-    question: "are you sure ?",
+    question: "Are you sure?",
     answers: [
-     { text: "blue", correct: yes },
-      { text: "grey", correct: ehh },
-      { text: "green", correct: eat shit }
+      { text: "Totally sure 💖", correct: true },
+      { text: "Ehh... maybe 😅", correct: false },
+      { text: "Not really 😬", correct: false }
     ]
   },
   {
-    question: "are you suuuure suuuuuure ?",
+    question: "Are you suuuure suuuuuure?",
     answers: [
-       { text: "30", correct: yes },
-      { text: "WAAAAAAAy tooo old", correct: yesss },
-      
+      { text: "Yes, 100% sure 💘", correct: true },
+      { text: "I guess so 🤔", correct: false },
+      { text: "Waaay too sure 😂", correct: false }
+    ]
+  }
+];
